5580b2b6c34768e9fe36deb006765bf72a74cf23
Tomer <gmtomer@gmail.com>
2015-12-15 10:24:54 +0200

    added main jsx

---
+++ app/app.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, Link } from 'react-router';
+import { reduxReactRouter, ReduxRouter, pushState } from 'redux-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import { createStore, compose } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { createHistory } from 'history';
+import searchApp from './redux/reducers';
+import { changeFiles } from './redux/actions';
+import Results from './views/results';
+import request from 'superagent';
+
+let store = compose(
+    reduxReactRouter({ createHistory })
+)(createStore)(searchApp);
+const dispatch = store.dispatch;
+
+class App extends React.Component {
+    static propTypes = {
+        children: React.PropTypes.node
+    }
+    constructor(props) {
+        super(props);
+    }
+    render () {
+        return (
+            <h1>FS</h1>
+        );
+    }
+}
+
+let Appx = connect(
+    // Use a selector to subscribe to state
+    state => ({
+        q: state.router.location.query.q,
+        files: state.files
+    }),
+    // Use an action creator for navigation
+    { pushState }
+)(App);
+
+let routes = (
+    <Route path="/" component={Appx}>
+    </Route>
+);
+
+class Root extends React.Component {
+    render() {
+        return (
+            <div>
+                <Provider store={store}>
+                    <ReduxRouter>
+                        {routes}
+                    </ReduxRouter>
+                </Provider>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Root />, document.getElementById('app'));
