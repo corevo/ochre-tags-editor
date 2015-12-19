72523a8319b9285ab062c0443edc1df933300f2e
Tomer <gmtomer@gmail.com>
2015-12-19 12:22:07 +0200

    now calls for files

--- app/views/explorer.jsx
+++ app/views/explorer.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import request from 'superagent';
 
 export default class Explorer extends React.Component {
     constructor(props) {
         super(props);
     }
-
+    componentWillReceiveProps(nextProps) {
+        if (this.props.location.pathname !== nextProps.location.pathname) {
+            request.get('/api' + nextProps.location.pathname).end((err, res) => {
+                if (!err) {
+                    this.props.setFiles(JSON.parse(res.text));
+                }
+            });
+        }
+    }
     render() {
         return (
             <div>
