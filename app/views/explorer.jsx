f64fd41a94e2e18bfe60e592fa4e80531e77fcc0
Tomer <gmtomer@gmail.com>
2015-12-19 16:33:22 +0200

    shows files

--- app/views/explorer.jsx
+++ app/views/explorer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import request from 'superagent';
 
 export default class Explorer extends React.Component {
@@ -13,7 +14,6 @@ export default class Explorer extends React.Component {
         if (this.props.location.pathname !== nextProps.location.pathname) {
             let path = '/api' + nextProps.location.pathname;
             request.get(path).end((err, res) => {
-                debugger;
                 if (!err) {
                     this.props.setFiles(JSON.parse(res.text));
                 }
@@ -21,18 +21,33 @@ export default class Explorer extends React.Component {
         }
     }
     render() {
+        let path = this.props.location.pathname;
+        if (path[path.length - 1] === '/')
+            path = path.substr(0, path.length - 1);
         return (
             <div>
-                <h1>{this.props.location.pathname.split("/").reduce((currLink, sublink, index) => {
+                <h1>{path.split("/").reduce((currLink, sublink, index) => {
                         if (index !== 0 && sublink === "")
                             return currLink;
                         currLink.location += `${sublink}/`;
-                        currLink.links.push(<a key={currLink.location} href={currLink.location}>{sublink}/</a>);
+                        currLink.links.push(<Link key={currLink.location} to={currLink.location}>{sublink}/</Link>);
                         return currLink;
                     }, {
                         location: "",
                         links: []
                     }).links}</h1>
+                <hr />
+                { this.props.files ?
+                    <ul style={{
+                            listStyle: 'none'
+                        }}>
+                        { this.props.files.map(file => (
+                            <li key={file}>
+                                <Link to={`${path}${file}`}>{file}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                : undefined }
             </div>
         );
     }
