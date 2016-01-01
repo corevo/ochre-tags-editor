1d516cc6bc5698f253be1a05b35e22c96c7c176f
Tomer <gmtomer@gmail.com>
2016-01-01 21:04:14 +0200

    added new loaders to support fonts

--- webpack.config.js
+++ webpack.config.js
@@ -15,18 +15,28 @@ module.exports = {
         index: './app/app'
     },
     output: {
-        filename: './public/assets/bin/[name].js'
+        filename: 'assets/bin/[name].js',
+        path: './public',
+        publicPath: '/'
     },
     module: {
         loaders: [
             {
-                test: /app(\/|\\).*\.(js||jsx)$/,
+                test: /app(\/|\\).*\.(js|jsx)$/,
                 loader: 'babel',
                 query: {
                     // https://github.com/babel/babel-loader#options
                     cacheDirectory: true,
                     presets: ['es2015', 'stage-0', 'react']
                 }
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader?name=assets/bin/[name].[ext]&limit=10000&minetype=application/font-woff"
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader?name=assets/bin/[name].[ext]"
             }
         ]
     },
