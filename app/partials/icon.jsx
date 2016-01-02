aea78fc40c453aa3911d7c995c37535cc8fa5847
Tomer <gmtomer@gmail.com>
2016-01-02 11:27:54 +0200

    added files ext hashtable

--- app/partials/icon.jsx
+++ app/partials/icon.jsx
@@ -6,6 +6,18 @@ export default class Icon extends React.Component {
         ext: React.PropTypes.string.isRequired,
         height: React.PropTypes.string
     }
+    fileType (ext) {
+        const types = {
+            ["pdf"]: ["pdf"],
+            ["excel"]: ["xls", "xlsx"],
+            ["word"]: ["doc", "docx", "rtf"],
+            ["text"]: ["txt", "log"],
+            ["image"]: ["jpg", "png", "bmp", "jpeg", "gif", "tif", "tiff", "svg"],
+            ["powerpoint"]: ["ppt", "pptx"],
+            ["video"]: ["mp4", "avi", "mkv", "mpeg"],
+            ["audio"]: ["mp3", "wav"]
+        }
+    }
     render () {
         return (
             <i className="fa fa-file-pdf-o" style={{
