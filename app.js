28951cbaab644d1a495d87914edfaf514ff58f2b
Tomer <gmtomer@gmail.com>
2016-01-25 14:40:51 +0200

    added tags route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import files from './lib/routes/files';
+import tags from './lib/routes/tags';
 
 let port = process.env.TAGS_PORT || 8080;
 let app = express();
@@ -8,7 +9,8 @@ let app = express();
 const STATIC_DIR = path.join(__dirname, 'public/assets');
 app.use('/assets', express.static(STATIC_DIR));
 
-app.use('/api', files);
+app.use('/api/files', files);
+app.use('/api/tags', tags);
 
 app.use('*', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
