528c4169d96e701a62cd100fce66acc30a6d4a0f
Tomer <gmtomer@gmail.com>
2016-05-30 11:08:52 +0300

    api workzzzz

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import files from './lib/routes/files';
 import tags from './lib/routes/tags';
+import elastic from './lib/routes/elastic';
 
 let port = process.env.TAGS_PORT || 8080;
 let app = express();
@@ -11,6 +12,7 @@ app.use('/assets', express.static(STATIC_DIR));
 
 app.use('/api/files', files);
 app.use('/api/tags', tags);
+app.use('/api/elastic', elastic);
 
 app.use('*', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
